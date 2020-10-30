ce7c0aeb8015f9949d5da7070592a741ca75ac7c
Daniel Crook <dacr5163@shas0136.rc.int.colorado.edu>
2020-10-30 00:08:32 -0600

    viz cleanup and setup dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='singRL',
       description='Meta-reinforcement learning for MIT TransportMaps' Sparsity Identification Algorithm (SING)',
       author='Daniel Crook',
       author_email='daniel.m.crook@colorado.edu',
-      install_requires=['gym>0.17.3','TransportMaps>2.0b3'],  # And any other dependencies foo needs
+      install_requires=['gym>0.17.3','TransportMaps>2.0b3','pickle5>0.0.11'],  # And any other dependencies foo needs
       python_requires='>=3.7'
 )
--- singRL/viz/genp_viz.py
+++ singRL/viz/genp_viz.py
@@ -2,33 +2,42 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
+# open data
 file_pi = open('genpArc.obj', 'rb')
 genp = pickle.load(file_pi)
 file_pi.close()
 
-
+# should match with gym call
 episodes = 4
 
 plt.figure()
 # episodes
 for z in range(0,episodes):
+
+    # get episode traits
     sz = genp[z].shape
     p = sz[0]
     timesteps = int(sz[1]/p)
 
+    # initialize bounds through timesteps
     low = 0
     high = p
+
     # timesteps in episode
     for t in range(0,timesteps):
+        # include all dimensions
         for dim in range(0,p):
             if dim == 0:
+                # initialize graph
                 graph = genp[z][dim][low:high]
             else:
                 graph = np.vstack((graph,genp[z][dim][low:high]))
 
+        # update timestep bounds
         low = high
         high = high+p
 
+        # update figure
         plt.imshow(graph)
         plt.colorbar()
         plt.pause(0.00001)
