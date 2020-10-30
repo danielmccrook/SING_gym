a8da17d3217076185132f5e625aa5803d29f7274
Daniel Crook <dacr5163@shas0136.rc.int.colorado.edu>
2020-10-29 18:22:35 -0600

    setup formalization

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup
 
 setup(name='singRL',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      description='Meta-reinforcement learning for MIT TransportMaps' Sparsity Identification Algorithm (SING)',
+      author='Daniel Crook',
+      author_email='daniel.m.crook@colorado.edu',
+      install_requires=['gym>0.17.3','TransportMaps>2.0b3'],  # And any other dependencies foo needs
+      python_requires='>=3.7'
 )
